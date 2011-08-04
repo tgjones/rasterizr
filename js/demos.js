497e867f42ba2250ed08e3904502f17a1d8fea5b
Tim Jones <tim@timjones.tw>
2011-08-05 03:35:42 +0800

    very basic output merging with no blend options

--- js/demos.js
+++ js/demos.js
@@ -1,27 +1,84 @@
-$(document).ready(function() {
-	var gridSize = { width: 300, height: 200 };
+var RasterizrDemos = { };
+
+RasterizrDemos.Grid = function(element, width, height, cellSize) {
+	this.width = width;
+	this.height = height;
+	this.cellSize = cellSize;
 	
-	var paper = Raphael("demo1", gridSize.width + 1, gridSize.height + 1);
-	
+	var paper = Raphael(element, width + 1, height + 1);
+
 	var drawLineFunc = function(path) {
 		paper.path(path).attr({
 			"stroke-width": 1,
 			"stroke": "#CCC"
 		});
 	}
-	
+
 	// Draw gridlines.
-	var cellSize = 20;
-	for (var x = 0; x <= gridSize.width; x += cellSize) {
-		drawLineFunc("M" + (x + 0.5) + " 0L" + (x + 0.5) + " " + gridSize.height);
+	for (var x = 0; x <= width; x += cellSize) {
+		drawLineFunc("M" + (x + 0.5) + " 0L" + (x + 0.5) + " " + height);
 	}
-	for (var y = 0; y <= gridSize.height; y += cellSize) {
-		drawLineFunc("M0 " + (y + 0.5) + "L" + gridSize.width + " " + (y + 0.5));
+	for (var y = 0; y <= height; y += cellSize) {
+		drawLineFunc("M0 " + (y + 0.5) + "L" + width + " " + (y + 0.5));
 	}
 	
-	// Now colour in some cells.
-	paper.rect(1, 1, cellSize - 1, cellSize - 1).attr({
-		"stroke-width": 0,
-		"fill": "red"
-	});
+	// Create cells.
+	var numCellsX = width / cellSize;
+	var numCellsY = height / cellSize;
+	var cells = new Array();
+	for (x = 0; x < numCellsX; x++) {
+		cells[x] = new Array();
+		for (y = 0; y < numCellsY; y++) {
+			cells[x][y] = paper.rect((x * cellSize) + 1, (y * cellSize) + 1, cellSize - 1, cellSize - 1).attr({
+				"stroke-width": 0,
+				"fill": "white"
+			});
+		}
+	}
+	
+	this.setCellColor = function(x, y, color) {
+		cells[x][y].attr({
+			"fill": color
+		})
+	};
+	
+	this.setCellColors = function(cells) {
+		for (var i = 0; i < cells.length; i++) {
+			var cell = cells[i];
+			this.setCellColor(cell.x, cell.y, cell.color);
+		}
+	}
+	
+	return this;
+};
+
+$(document).ready(function() {
+	var cellSize = 20;
+	
+	var grid1 = new RasterizrDemos.Grid("demo1", 300, 200, cellSize);
+	var renderTargetPixels = new Array();
+	for (x = 3; x < 8; x++) {
+		for (y = 1; y < 8; y++) {
+			renderTargetPixels.push({ x: x, y: y, color: "red" });
+		}
+	}
+	grid1.setCellColors(renderTargetPixels);
+	
+	var grid2 = new RasterizrDemos.Grid("demo2", 300, 200, cellSize);
+	var pixels = new Array();
+	for (x = 1; x < 14; x++) {
+		for (y = 3; y < 6; y++) {
+			pixels.push({ x: x, y: y, color: "green" });
+		}
+	}
+	grid2.setCellColors(pixels);
+	
+	// Now apply some logic to calculate the result colours.
+	var grid3 = new RasterizrDemos.Grid("demo3", 300, 200, cellSize);
+	grid3.setCellColors(renderTargetPixels);
+	for (var i = 0; i < pixels.length; i++) {
+		var pixel = pixels[i];
+		grid3.setCellColor(pixel.x, pixel.y, pixel.color);
+	}
+	
 });
