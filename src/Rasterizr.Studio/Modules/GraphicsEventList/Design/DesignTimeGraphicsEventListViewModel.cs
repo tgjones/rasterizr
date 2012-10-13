9cf65fadbde4f92291a80a20c94a21bd131e4e3c
Tim Jones <tim@timjones.tw>
2012-10-13 23:28:00 +0800

    Add WPF design-time support for event list

---
+++ src/Rasterizr.Studio/Modules/GraphicsEventList/Design/DesignTimeGraphicsEventListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Rasterizr.Diagnostics.Logging;
+using Rasterizr.Studio.Modules.GraphicsDebugging.Design;
+using Rasterizr.Studio.Modules.GraphicsDebugging.ViewModels;
+using Rasterizr.Studio.Modules.GraphicsEventList.ViewModels;
+
+namespace Rasterizr.Studio.Modules.GraphicsEventList.Design
+{
+	public class DesignTimeGraphicsEventListViewModel : GraphicsEventListViewModel
+	{
+		public DesignTimeGraphicsEventListViewModel()
+			: base(new DesignTimeSelectionService())
+		{
+			Events.Add(new TracefileEventViewModel(new TracefileEvent
+			{
+				Number = 1,
+				OperationType = Core.Diagnostics.OperationType.InputAssemblerSetVertices
+			}));
+			Events.Add(new TracefileEventViewModel(new TracefileEvent
+			{
+				Number = 2,
+				OperationType = Core.Diagnostics.OperationType.DeviceDraw
+			}));
+		}
+	}
+}
